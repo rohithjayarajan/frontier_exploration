fb8f3bac00906f6333584303d62194e029e9f3a3
rohithjayarajan <rohith.jayarajan@gmail.com>
2018-12-04 21:44:28 -0500

    [Task 2.5: STB_MS]: add stubs for MapStructure class
    design stubs consistent with the class diagram in revision1 and add
    the header and source file for MapStructure class

---
+++ include/frontier_exploration/map_structure.hpp
@@ -0,0 +1,196 @@
+/******************************************************************************
+ *                          GNU GENERAL PUBLIC LICENSE
+ *                           Version 3, 29 June 2007
+ *
+ * Copyright (C) 2018, Rohith Jayarajan
+ *****************************************************************************/
+/**
+ *  @file    map_structure.hpp
+ *  @author  rohithjayarajan
+ *  @date 12/2/2018
+ *  @version 0.1
+ *
+ *  @brief header file for MapStructure class
+ *
+ *  @section DESCRIPTION
+ *
+ *  header file which contains the declaration of MapStructure class
+ *
+ */
+
+#ifndef INCLUDE_FRONTIER_EXPLORATION_MAP_STRUCTURE_HPP_
+#define INCLUDE_FRONTIER_EXPLORATION_MAP_STRUCTURE_HPP_
+
+#include <geometry_msgs/PoseStamped.h>
+#include <vector>
+/**
+ * @brief the declaration of MapStructure class
+ *
+ * Declaration of the variable and methods of MapStructure class
+ */
+class MapStructure {
+ private:
+  unsigned int resolution;
+  unsigned int width;
+  unsigned int height;
+  geometry_msgs::Pose origin;
+  bool isOnFrontier;
+  unsigned int frontierClass;
+  int x;
+  int y;
+  double data;
+
+ public:
+  /**
+   *   @brief Default constructor for MapStructure
+   *
+   *   @param nothing
+   *   @return nothing
+   */
+  MapStructure();
+  /**
+   *   @brief custom constructor for MapStructure
+   *
+   *   @param unsigned int value for resolution
+   *   @param unsigned int value for width
+   *   @param unsigned int value for height
+   *   @param nothgeometry_msgs::Pose value for origin
+   *   @return nothing
+   */
+  MapStructure(unsigned const int &resolution, unsigned const int &width,
+               unsigned const int &height, const geometry_msgs::Pose &origin);
+  /**
+   *   @brief Default destructor for MapStructure
+   *
+   *   @param nothing
+   *   @return nothing
+   */
+  ~MapStructure();
+  /**
+   *   @brief setter for resolution variable
+   *
+   *   @param unsigned int value of width resolution
+   *   @return nothing
+   */
+  void setResolution(unsigned const int &resolution_);
+  /**
+   *   @brief setter for width variable
+   *
+   *   @param unsigned int value of width
+   *   @return nothing
+   */
+  void setWidth(unsigned const int &width_);
+  /**
+   *   @brief setter for height variable
+   *
+   *   @param unsigned int value of height
+   *   @return nothing
+   */
+  void setHeight(unsigned const int &height_);
+  /**
+   *   @brief setter for origin variable
+   *
+   *   @param geometry_msgs::Pose value of origin
+   *   @return nothing
+   */
+  void setOrigin(const geometry_msgs::Pose &origin_);
+  /**
+   *   @brief setter for isOnFrontier variable
+   *
+   *   @param bool value of isOnFrontier
+   *   @return nothing
+   */
+  void setIsOnFrontier(const bool &isOnFrontier_);
+  /**
+   *   @brief setter for frontierClass variable
+   *
+   *   @param unsigned int value of frontierClass
+   *   @return nothing
+   */
+  void setFrontierClass(unsigned const int &frontierClass_);
+  /**
+   *   @brief setter for map coordinate x variable
+   *
+   *   @param int value of x coordinate
+   *   @return nothing
+   */
+  void setMapXCoordinate(const int &x_);
+  /**
+   *   @brief setter for map coordinate y variable
+   *
+   *   @param int value of y coordinate
+   *   @return nothing
+   */
+  void setMapYCoordinate(const int &y_);
+  /**
+   *   @brief setter for map data
+   *
+   *   @param double value of map data
+   *   @return nothing
+   */
+  void setMapData(const double &data_);
+  /**
+   *   @brief getter for resolution variable
+   *
+   *   @param nothing
+   *   @return unsigned int value of width resolution
+   */
+  unsigned int getResolution();
+  /**
+   *   @brief getter for width variable
+   *
+   *   @param nothing
+   *   @return unsigned int value of width
+   */
+  unsigned int getWidth();
+  /**
+   *   @brief getter for height variable
+   *
+   *   @param nothing
+   *   @return unsigned int value of height
+   */
+  unsigned int getHeight();
+  /**
+   *   @brief getter for origin variable
+   *
+   *   @param nothing
+   *   @return geometry_msgs::Pose value of origin
+   */
+  geometry_msgs::Pose getOrigin();
+  /**
+   *   @brief getter for isOnFrontier variable
+   *
+   *   @param nothing
+   *   @return bool value of isOnFrontier
+   */
+  bool getIsOnFrontier();
+  /**
+   *   @brief getter for frontierClass variable
+   *
+   *   @param nothing
+   *   @return unsigned int value of frontierClass
+   */
+  unsigned int getFrontierClass();
+  /**
+   *   @brief getter for map coordinate x variable
+   *
+   *   @param nothing
+   *   @return int value of x coordinate
+   */
+  int getMapXCoordinate();
+  /**
+   *   @brief getter for map coordinate y variable
+   *
+   *   @param nothing
+   *   @return int value of y coordinate
+   */
+  int getMapYCoordinate();
+  /**
+   *   @brief getter for map data
+   *
+   *   @param nothing
+   *   @return double value of map data
+   */
+  double getMapData();
+};
+#endif  // INCLUDE_FRONTIER_EXPLORATION_MAP_STRUCTURE_HPP_
---
+++ src/map_structure.cpp
@@ -0,0 +1,175 @@
+/******************************************************************************
+ *                          GNU GENERAL PUBLIC LICENSE
+ *                           Version 3, 29 June 2007
+ *
+ * Copyright (C) 2018, Rohith Jayarajan
+ *****************************************************************************/
+/**
+ *  @file    map_structure.cpp
+ *  @author  rohithjayarajan
+ *  @date 12/2/2018
+ *  @version 0.1
+ *
+ *  @brief source file for MapStructure class
+ *
+ *  @section DESCRIPTION
+ *
+ *  header file which contains the definiton of MapStructure class
+ */
+#include "map_structure.hpp"
+/**
+ *   @brief Default constructor for MapStructure
+ *
+ *   @param nothing
+ *   @return nothing
+ */
+MapStructure::MapStructure() {}
+/**
+ *   @brief custom constructor for MapStructure
+ *
+ *   @param unsigned int value for resolution
+ *   @param unsigned int value for width
+ *   @param unsigned int value for height
+ *   @param nothgeometry_msgs::Pose value for origin
+ *   @return nothing
+ */
+MapStructure::MapStructure(unsigned const int &resolution,
+                           unsigned const int &width,
+                           unsigned const int &height,
+                           const geometry_msgs::Pose &origin) {}
+/**
+ *   @brief Default destructor for MapStructure
+ *
+ *   @param nothing
+ *   @return nothing
+ */
+MapStructure::~MapStructure() {}
+/**
+ *   @brief setter for resolution variable
+ *
+ *   @param unsigned int value of width resolution
+ *   @return nothing
+ */
+void MapStructure::setResolution(unsigned const int &resolution_) {}
+/**
+ *   @brief setter for width variable
+ *
+ *   @param unsigned int value of width
+ *   @return nothing
+ */
+void MapStructure::setWidth(unsigned const int &width_) {}
+/**
+ *   @brief setter for height variable
+ *
+ *   @param unsigned int value of height
+ *   @return nothing
+ */
+void MapStructure::setHeight(unsigned const int &height_) {}
+/**
+ *   @brief setter for origin variable
+ *
+ *   @param geometry_msgs::Pose value of origin
+ *   @return nothing
+ */
+void MapStructure::setOrigin(const geometry_msgs::Pose &origin_) {}
+/**
+ *   @brief setter for isOnFrontier variable
+ *
+ *   @param bool value of isOnFrontier
+ *   @return nothing
+ */
+void MapStructure::setIsOnFrontier(const bool &isOnFrontier_) {}
+/**
+ *   @brief setter for frontierClass variable
+ *
+ *   @param unsigned int value of frontierClass
+ *   @return nothing
+ */
+void MapStructure::setFrontierClass(unsigned const int &frontierClass_) {}
+/**
+ *   @brief setter for map coordinate x variable
+ *
+ *   @param int value of x coordinate
+ *   @return nothing
+ */
+void MapStructure::setMapXCoordinate(const int &x_) {}
+/**
+ *   @brief setter for map coordinate y variable
+ *
+ *   @param int value of y coordinate
+ *   @return nothing
+ */
+void MapStructure::setMapYCoordinate(const int &y_) {}
+/**
+ *   @brief setter for map data
+ *
+ *   @param double value of map data
+ *   @return nothing
+ */
+void MapStructure::setMapData(const double &data_) {}
+/**
+ *   @brief getter for resolution variable
+ *
+ *   @param nothing
+ *   @return unsigned int value of width resolution
+ */
+unsigned int MapStructure::getResolution() { return 0; }
+/**
+ *   @brief getter for width variable
+ *
+ *   @param nothing
+ *   @return unsigned int value of width
+ */
+unsigned int MapStructure::getWidth() { return 0; }
+/**
+ *   @brief getter for height variable
+ *
+ *   @param nothing
+ *   @return unsigned int value of height
+ */
+unsigned int MapStructure::getHeight() { return 0; }
+/**
+ *   @brief getter for origin variable
+ *
+ *   @param nothing
+ *   @return geometry_msgs::Pose value of origin
+ */
+geometry_msgs::Pose MapStructure::getOrigin() {
+  geometry_msgs::Pose a;
+  return a;
+}
+/**
+ *   @brief getter for isOnFrontier variable
+ *
+ *   @param nothing
+ *   @return bool value of isOnFrontier
+ */
+bool MapStructure::getIsOnFrontier() { return true; }
+/**
+ *   @brief getter for frontierClass variable
+ *
+ *   @param nothing
+ *   @return unsigned int value of frontierClass
+ */
+unsigned int MapStructure::getFrontierClass() { return 0; }
+/**
+ *   @brief getter for map coordinate x variable
+ *
+ *   @param nothing
+ *   @return int value of x coordinate
+ */
+int MapStructure::getMapXCoordinate() { return 0; }
+/**
+ *   @brief getter for map coordinate y variable
+ *
+ *   @param nothing
+ *   @return int value of y coordinate
+ */
+int MapStructure::getMapYCoordinate() { return 0; }
+/**
+ *   @brief getter for map data
+ *
+ *   @param nothing
+ *   @return double value of map data
+ */
+double MapStructure::getMapData() { return 0.0; }
