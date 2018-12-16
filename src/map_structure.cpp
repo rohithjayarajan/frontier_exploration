81fb981cf98ea358ca83d6d241d6a94083eff008
rohithjayarajan <rohith.jayarajan@gmail.com>
2018-12-16 04:06:37 -0500

    [Task 3.5: IMPL_MS]: add implemention of MapStructure class
    add implementation of MapStructure class

--- src/map_structure.cpp
+++ src/map_structure.cpp
@@ -7,7 +7,7 @@
 /**
  *  @file    map_structure.cpp
  *  @author  rohithjayarajan
- *  @date 14/2/2018
+ *  @date 16/2/2018
  *  @version 1.1
  *
  *  @brief source file for MapStructure class
@@ -25,21 +25,45 @@ MapStructure::MapStructure() {}
 
 MapStructure::~MapStructure() {}
 
-void MapStructure::setIsOnFrontier(const bool &isOnFrontier_) {}
-
-void MapStructure::setFrontierClass(const int &frontierClass_) {}
-
-void MapStructure::setMapXYCoordinate(const uint32_t &x_, const uint32_t &y_) {}
-
-void MapStructure::setMapData(const int8_t &data_) {}
-
-bool MapStructure::getIsOnFrontier() { return false; }
-
-int MapStructure::getFrontierClass() { return 1; }
-
-std::pair<uint32_t, uint32_t> MapStructure::getMapXYCoordinate() {
-  std::pair<uint32_t, uint32_t> a;
-  return a;
+void MapStructure::setIsOnFrontier(const bool &isOnFrontier_) {
+  // set the value of variable isOnFrontier
+  isOnFrontier = isOnFrontier_;
 }
 
-int8_t MapStructure::getMapData() { return 1; }
+void MapStructure::setFrontierClass(const int &frontierClass_) {
+  // set the value of variable frontierClass
+  frontierClass = frontierClass_;
+}
+
+void MapStructure::setMapXYCoordinate(const uint32_t &x_, const uint32_t &y_) {
+  // set the value of variable x
+  x = x_;
+  // set the value of variable y
+  y = y_;
+}
+
+void MapStructure::setMapData(const int8_t &data_) {
+  // set the value of variable data
+  data = data_;
+}
+
+bool MapStructure::getIsOnFrontier() {
+  // return the value of variable isOnFrontier
+  return isOnFrontier;
+}
+
+int MapStructure::getFrontierClass() {
+  // return the value of variable frontierClass
+  return frontierClass;
+}
+
+std::pair<uint32_t, uint32_t> MapStructure::getMapXYCoordinate() {
+  std::pair<uint32_t, uint32_t> XYCoordinate(x, y);
+  // return the value of variable XYCoordinate
+  return XYCoordinate;
+}
+
+int8_t MapStructure::getMapData() {
+  // return the value of variable data
+  return data;
+}
