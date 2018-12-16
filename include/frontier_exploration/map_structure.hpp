8876b26290e217b22edede560d13aaf686143ff4
rohithjayarajan <rohith.jayarajan@gmail.com>
2018-12-16 01:33:03 -0500

    [Task 3.2: UPDT_MS]: update design of MapStructure Class
    update the design of MapStructure class, minor modifications in the
    return type and type of public variables to comply with ROS format

--- include/frontier_exploration/map_structure.hpp
+++ include/frontier_exploration/map_structure.hpp
@@ -7,8 +7,8 @@
 /**
  *  @file    map_structure.hpp
  *  @author  rohithjayarajan
- *  @date 12/2/2018
- *  @version 0.1
+ *  @date 14/2/2018
+ *  @version 1.1
  *
  *  @brief header file for MapStructure class
  *
@@ -21,8 +21,8 @@
 #ifndef INCLUDE_FRONTIER_EXPLORATION_MAP_STRUCTURE_HPP_
 #define INCLUDE_FRONTIER_EXPLORATION_MAP_STRUCTURE_HPP_
 
-#include <geometry_msgs/PoseStamped.h>
-#include <vector>
+#include <cstdint>
+#include <utility>
 /**
  * @brief the declaration of MapStructure class
  *
@@ -30,15 +30,16 @@
  */
 class MapStructure {
  private:
-  double resolution;
-  unsigned int width;
-  unsigned int height;
-  geometry_msgs::Pose origin;
+  // variable which tells if the grid cell is on a frontier
   bool isOnFrontier;
-  unsigned int frontierClass;
-  int x;
-  int y;
-  double data;
+  // variable which gives the class number of the frontier
+  int frontierClass;
+  // variable to hold x value of grid poosition
+  uint32_t x;
+  // variable to hold x value of grid poosition
+  uint32_t y;
+  // variable to hold probability value of obstacle given by occupancy grid
+  int8_t data;
 
  public:
   /**
@@ -49,51 +50,12 @@ class MapStructure {
    */
   MapStructure();
   /**
-   *   @brief custom constructor for MapStructure
-   *
-   *   @param unsigned int value for resolution
-   *   @param unsigned int value for width
-   *   @param unsigned int value for height
-   *   @param nothgeometry_msgs::Pose value for origin
-   *   @return nothing
-   */
-  MapStructure(const double &resolution, unsigned const int &width,
-               unsigned const int &height, const geometry_msgs::Pose &origin);
-  /**
    *   @brief Default destructor for MapStructure
    *
    *   @param nothing
    *   @return nothing
    */
   ~MapStructure();
-  /**
-   *   @brief setter for resolution variable
-   *
-   *   @param double value of resolution
-   *   @return nothing
-   */
-  void setResolution(const double &resolution_);
-  /**
-   *   @brief setter for width variable
-   *
-   *   @param unsigned int value of width
-   *   @return nothing
-   */
-  void setWidth(unsigned const int &width_);
-  /**
-   *   @brief setter for height variable
-   *
-   *   @param unsigned int value of height
-   *   @return nothing
-   */
-  void setHeight(unsigned const int &height_);
-  /**
-   *   @brief setter for origin variable
-   *
-   *   @param geometry_msgs::Pose value of origin
-   *   @return nothing
-   */
-  void setOrigin(const geometry_msgs::Pose &origin_);
   /**
    *   @brief setter for isOnFrontier variable
    *
@@ -104,59 +66,24 @@ class MapStructure {
   /**
    *   @brief setter for frontierClass variable
    *
-   *   @param unsigned int value of frontierClass
+   *   @param int value of frontierClass
    *   @return nothing
    */
-  void setFrontierClass(unsigned const int &frontierClass_);
+  void setFrontierClass(const int &frontierClass_);
   /**
-   *   @brief setter for map coordinate x variable
+   *   @brief setter for map coordinate x and y variable
    *
-   *   @param int value of x coordinate
+   *   @param uint32_t values of x and y coordinate
    *   @return nothing
    */
-  void setMapXCoordinate(const int &x_);
-  /**
-   *   @brief setter for map coordinate y variable
-   *
-   *   @param int value of y coordinate
-   *   @return nothing
-   */
-  void setMapYCoordinate(const int &y_);
+  void setMapXYCoordinate(const uint32_t &x_, const uint32_t &y_);
   /**
    *   @brief setter for map data
    *
-   *   @param double value of map data
+   *   @param int8_t value of map data
    *   @return nothing
    */
-  void setMapData(const double &data_);
-  /**
-   *   @brief getter for resolution variable
-   *
-   *   @param nothing
-   *   @return double value of resolution
-   */
-  double getResolution();
-  /**
-   *   @brief getter for width variable
-   *
-   *   @param nothing
-   *   @return unsigned int value of width
-   */
-  unsigned int getWidth();
-  /**
-   *   @brief getter for height variable
-   *
-   *   @param nothing
-   *   @return unsigned int value of height
-   */
-  unsigned int getHeight();
-  /**
-   *   @brief getter for origin variable
-   *
-   *   @param nothing
-   *   @return geometry_msgs::Pose value of origin
-   */
-  geometry_msgs::Pose getOrigin();
+  void setMapData(const int8_t &data_);
   /**
    *   @brief getter for isOnFrontier variable
    *
@@ -168,29 +95,22 @@ class MapStructure {
    *   @brief getter for frontierClass variable
    *
    *   @param nothing
-   *   @return unsigned int value of frontierClass
+   *   @return int value of frontierClass
    */
-  unsigned int getFrontierClass();
+  int getFrontierClass();
   /**
-   *   @brief getter for map coordinate x variable
+   *   @brief getter for map coordinate x and y variable
    *
    *   @param nothing
-   *   @return int value of x coordinate
+   *   @return std::pair<uint32_t, uint32_t> value of x and y coordinate
    */
-  int getMapXCoordinate();
-  /**
-   *   @brief getter for map coordinate y variable
-   *
-   *   @param nothing
-   *   @return int value of y coordinate
-   */
-  int getMapYCoordinate();
+  std::pair<uint32_t, uint32_t> getMapXYCoordinate();
   /**
    *   @brief getter for map data
    *
    *   @param nothing
-   *   @return double value of map data
+   *   @return int8_t value of map data
    */
-  double getMapData();
+  int8_t getMapData();
 };
 #endif  // INCLUDE_FRONTIER_EXPLORATION_MAP_STRUCTURE_HPP_
--- src/map_structure.cpp
+++ src/map_structure.cpp
@@ -7,168 +7,39 @@
 /**
  *  @file    map_structure.cpp
  *  @author  rohithjayarajan
- *  @date 12/2/2018
- *  @version 0.1
+ *  @date 14/2/2018
+ *  @version 1.1
  *
  *  @brief source file for MapStructure class
  *
  *  @section DESCRIPTION
  *
- *  header file which contains the definiton of MapStructure class
+ *  source file which contains the defintion of MapStructure class
+ *
  */
 #include "map_structure.hpp"
-/**
- *   @brief Default constructor for MapStructure
- *
- *   @param nothing
- *   @return nothing
- */
+#include <cstdint>
+#include <utility>
+
 MapStructure::MapStructure() {}
-/**
- *   @brief custom constructor for MapStructure
- *
- *   @param unsigned int value for resolution
- *   @param unsigned int value for width
- *   @param unsigned int value for height
- *   @param nothgeometry_msgs::Pose value for origin
- *   @return nothing
- */
-MapStructure::MapStructure(const double &resolution, unsigned const int &width,
-                           unsigned const int &height,
-                           const geometry_msgs::Pose &origin) {}
-/**
- *   @brief Default destructor for MapStructure
- *
- *   @param nothing
- *   @return nothing
- */
+
 MapStructure::~MapStructure() {}
-/**
- *   @brief setter for resolution variable
- *
- *   @param double value of resolution
- *   @return nothing
- */
-void MapStructure::setResolution(const double &resolution_) {}
-/**
- *   @brief setter for width variable
- *
- *   @param unsigned int value of width
- *   @return nothing
- */
-void MapStructure::setWidth(unsigned const int &width_) {}
-/**
- *   @brief setter for height variable
- *
- *   @param unsigned int value of height
- *   @return nothing
- */
-void MapStructure::setHeight(unsigned const int &height_) {}
-/**
- *   @brief setter for origin variable
- *
- *   @param geometry_msgs::Pose value of origin
- *   @return nothing
- */
-void MapStructure::setOrigin(const geometry_msgs::Pose &origin_) {}
-/**
- *   @brief setter for isOnFrontier variable
- *
- *   @param bool value of isOnFrontier
- *   @return nothing
- */
+
 void MapStructure::setIsOnFrontier(const bool &isOnFrontier_) {}
-/**
- *   @brief setter for frontierClass variable
- *
- *   @param unsigned int value of frontierClass
- *   @return nothing
- */
-void MapStructure::setFrontierClass(unsigned const int &frontierClass_) {}
-/**
- *   @brief setter for map coordinate x variable
- *
- *   @param int value of x coordinate
- *   @return nothing
- */
-void MapStructure::setMapXCoordinate(const int &x_) {}
-/**
- *   @brief setter for map coordinate y variable
- *
- *   @param int value of y coordinate
- *   @return nothing
- */
-void MapStructure::setMapYCoordinate(const int &y_) {}
-/**
- *   @brief setter for map data
- *
- *   @param double value of map data
- *   @return nothing
- */
-void MapStructure::setMapData(const double &data_) {}
-/**
- *   @brief getter for resolution variable
- *
- *   @param nothing
- *   @return unsigned int value of resolution
- */
-double MapStructure::getResolution() { return 0.0; }
-/**
- *   @brief getter for width variable
- *
- *   @param nothing
- *   @return unsigned int value of width
- */
-unsigned int MapStructure::getWidth() { return 0; }
-/**
- *   @brief getter for height variable
- *
- *   @param nothing
- *   @return unsigned int value of height
- */
-unsigned int MapStructure::getHeight() { return 0; }
-/**
- *   @brief getter for origin variable
- *
- *   @param nothing
- *   @return geometry_msgs::Pose value of origin
- */
-geometry_msgs::Pose MapStructure::getOrigin() {
-  geometry_msgs::Pose a;
+
+void MapStructure::setFrontierClass(const int &frontierClass_) {}
+
+void MapStructure::setMapXYCoordinate(const uint32_t &x_, const uint32_t &y_) {}
+
+void MapStructure::setMapData(const int8_t &data_) {}
+
+bool MapStructure::getIsOnFrontier() { return false; }
+
+int MapStructure::getFrontierClass() { return 1; }
+
+std::pair<uint32_t, uint32_t> MapStructure::getMapXYCoordinate() {
+  std::pair<uint32_t, uint32_t> a;
   return a;
 }
-/**
- *   @brief getter for isOnFrontier variable
- *
- *   @param nothing
- *   @return bool value of isOnFrontier
- */
-bool MapStructure::getIsOnFrontier() { return true; }
-/**
- *   @brief getter for frontierClass variable
- *
- *   @param nothing
- *   @return unsigned int value of frontierClass
- */
-unsigned int MapStructure::getFrontierClass() { return 0; }
-/**
- *   @brief getter for map coordinate x variable
- *
- *   @param nothing
- *   @return int value of x coordinate
- */
-int MapStructure::getMapXCoordinate() { return 0; }
-/**
- *   @brief getter for map coordinate y variable
- *
- *   @param nothing
- *   @return int value of y coordinate
- */
-int MapStructure::getMapYCoordinate() { return 0; }
-/**
- *   @brief getter for map data
- *
- *   @param nothing
- *   @return double value of map data
- */
-double MapStructure::getMapData() { return 0.0; }
+
+int8_t MapStructure::getMapData() { return 1; }
